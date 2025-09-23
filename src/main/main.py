33ca1e8c8d0968a579d872f69b334ec478dd44ab
Oldranda <leonardorandacio@gmail.com>
2025-09-23 13:59:42 +0200

    add second era and finish incomplete card registry

--- src/main/main.py
+++ src/main/main.py
@@ -1,15 +1,9 @@
 from loader import load_cards
 
 def main():
-    era = 1
-    era2_path = "./assets/era2_cards.json"
-    era3_path = "./assets/era3_cards.json"
-    # era2_cards = load_cards(era2_path) 
-    # era3_cards = load_cards(era3_path) 
+    era = 2
     for i in range (3, 8):
         print(f"era 1 cards for {i} players count: {len(load_cards(era, i))}. should be {7*i}")
-    # print(era2_cards[0])
-    # print(era3_cards[-1])
 
 if __name__ == "__main__":
     main()
