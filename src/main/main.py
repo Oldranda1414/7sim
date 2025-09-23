47cb3a1a8976798d9b1efd190934e48329629ec9
Oldranda <leonardorandacio@gmail.com>
2025-09-23 14:38:30 +0200

    add third era incomplete cards

--- src/main/main.py
+++ src/main/main.py
@@ -1,9 +1,12 @@
 from loader import load_cards
 
 def main():
-    era = 2
-    for i in range (3, 8):
-        print(f"era 1 cards for {i} players count: {len(load_cards(era, i))}. should be {7*i}")
+    for era in range(1,4):
+        for i in range (3, 8):
+            if era == 3:
+                print(f"era {era} cards for {i} players count: {len(load_cards(era, i)) + 2+i}. should be {7*i}")
+            else:
+                print(f"era {era} cards for {i} players count: {len(load_cards(era, i))}. should be {7*i}")
 
 if __name__ == "__main__":
     main()
