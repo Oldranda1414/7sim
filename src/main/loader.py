23cca939f5e3217cc68579175499cb21b80485c0
Oldranda <leonardorandacio@gmail.com>
2025-09-23 12:06:06 +0200

    improve json for incomplete first era

--- src/main/loader.py
+++ src/main/loader.py
@@ -1,15 +1,20 @@
 import json
+import copy
+
 from card import Card
 from card_type import Type
 from game_resource import Resource
 from effect import Effect
 from icon import Icon
 
-def load_cards(path: str) -> list[Card]:
-    with open(path, "r", encoding="utf-8") as f:
+CARD_REGISTRY_PATH = "./src/main/assets/cards.json"
+ERA_REGISTRY_PATH = "./src/main/assets/era"
+
+def load_cards(era: int, player_number: int) -> list[Card]:
+    with open(CARD_REGISTRY_PATH, "r", encoding="utf-8") as f:
         data = json.load(f)
 
-    cards = []
+    card_registry: dict[str, Card] = {}
     for c in data["cards"]:
         # Parse cost as (money, [Resource,...])
         cost_data = c.get("cost", {"money": 0, "resources": []})
@@ -17,17 +22,27 @@ def load_cards(path: str) -> list[Card]:
             cost_data.get("money", 0),
             [Resource[r] for r in cost_data.get("resources", [])]
         )
+        name = c["name"]
 
-        cards.append(
-            Card(
-                name=c["name"],
+        card_registry[name] = Card(
+                name=name,
                 card_type=Type[c["type"]],
                 effect=Effect(),
                 cost=cost,
                 give_icon=[Icon[i] for i in c.get("give_icon", [])],
                 require_icon=Icon[c["require_icon"]] if c.get("require_icon") else None,
-                era=c["era"],
-                player_number=c["player_number"],
             )
-        )
+
+    with open(_get_era_resitry_path(era), "r", encoding="utf-8") as f:
+        data = json.load(f)
+
+    cards: list[Card] = []
+    for card in data["era"]:
+        name = card["name"]
+        number = card["number"][player_number - 3]
+        for _ in range(number):
+            cards.append(copy.deepcopy(card_registry[name]))
     return cards
+
+def _get_era_resitry_path(era_number: int) -> str:
+    return f"{ERA_REGISTRY_PATH}/{era_number}.json"
--- src/main/main.py
+++ src/main/main.py
@@ -1,15 +1,13 @@
 from loader import load_cards
 
 def main():
-    era1_path = "./src/main/assets/era1_cards.json"
+    era = 1
     era2_path = "./assets/era2_cards.json"
     era3_path = "./assets/era3_cards.json"
-    era1_cards = load_cards(era1_path) 
     # era2_cards = load_cards(era2_path) 
     # era3_cards = load_cards(era3_path) 
-    print("era 1 cards total:",len(era1_cards), "should be 49")
     for i in range (3, 8):
-        print(f"era 1 cards for {i} players count: {len([card for card in era1_cards if card.player_number <= i])}. should be {7*i}")
+        print(f"era 1 cards for {i} players count: {len(load_cards(era, i))}. should be {7*i}")
     # print(era2_cards[0])
     # print(era3_cards[-1])
 
